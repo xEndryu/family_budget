b99da57f1279cbe6739eb2e43feaa371a7942c76
Andrzej Laszeski <endryuzwa@gmail.com>
2022-12-13 11:36:09 +0100

    Added Alembic to support database migrations

---
+++ alembic/versions/f19d39c34348_alembic_init.py
@@ -0,0 +1,24 @@
+"""alembic init
+
+Revision ID: f19d39c34348
+Revises: 
+Create Date: 2022-12-13 11:02:09.172380
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f19d39c34348'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
